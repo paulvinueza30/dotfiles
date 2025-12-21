aa3dc8a1e151881033a3f74e065ebc6893112a99
paul vinueza <paulvinueza30@gmail.com>
2025-12-20 19:05:54 -0500

    less startup bc ram dying

--- hypr/.config/hypr/startup-webapps.sh
+++ hypr/.config/hypr/startup-webapps.sh
@@ -5,11 +5,7 @@ webapp="omarchy-launch-or-focus-webapp --profile-directory=Default"
 urls=(
   "https://x.com/"
   "https://www.linkedin.com/feed/"
-  "https://youtube.com/"
   "https://github.com/"
-  "https://twitch.tv/"
-  "https://mail.google.com"
-  "https://neetcode.io"
   "https://gemini.google.com/app"
   "http://10.0.0.72:8006/#v1:0:=node%2Fpve:4:5::::::"
 )
