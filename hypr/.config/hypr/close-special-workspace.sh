81de835389bcd58b3e52584399e2ef550e675f10
paul vinueza <paulvinueza30@gmail.com>
2025-12-23 17:14:50 -0500

    close special workspace on switch

---
+++ hypr/.config/hypr/close-special-workspace.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+active=$(hyprctl -j monitors | jq --raw-output '.[] | select(.focused==true).specialWorkspace.name | split(":") | if length > 1 then .[1] else "" end')
+
+if [[ ${#active} -gt 0 ]]; then
+  hyprctl dispatch togglespecialworkspace "$active"
+fi
