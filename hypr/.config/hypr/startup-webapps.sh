92336008223a1dd685451840ca01aee3e2950720
paul vinueza <paulvinueza30@gmail.com>
2026-03-18 22:15:21 -0400

    add webapps and specialworkspaces, change default keybinds for group window nav, rename special workspaces

--- hypr/.config/hypr/startup-webapps.sh
+++ hypr/.config/hypr/startup-webapps.sh
@@ -3,11 +3,15 @@
 webapp="omarchy-launch-or-focus-webapp --profile-directory=Default"
 
 urls=(
+  "https://discord.com/channels/@me"
   "https://x.com/"
   "https://www.linkedin.com/feed/"
   "https://github.com/"
-  "https://gemini.google.com/app"
+  "https://todoist.com/"
   "http://10.0.0.72:8006/#v1:0:=node%2Fpve:4:5::::::"
+  "https://www.instagram.com/"
+  "https://app.hey.com/calendar/weeks/"
+  "https://gemini.google.com/app"
 )
 
 for url in "${urls[@]}"; do
